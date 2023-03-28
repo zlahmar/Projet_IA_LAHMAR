2ff7ee61b924c8b912b88b96e554650c589f499a
zlahmar <114932730+zlahmar@users.noreply.github.com>
2023-03-28 22:56:32 +0200

    Initial commit

---
+++ app/src/main/java/fr/epsi/projet_ia_lahmar/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.epsi.projet_ia_lahmar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
